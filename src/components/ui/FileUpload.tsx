3057418d35d028e8162deca74cbc8e22e234eec1
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-02 01:40:49 +0600

    [ADD] s3 file upload

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -2,13 +2,25 @@
 import React from 'react'
 import { useDropzone } from 'react-dropzone';
 import { Inbox } from "lucide-react"
+import { uploadToS3 } from '@/lib/s3';
 
 export default function FileUpload() {
 	const { getRootProps, getInputProps } = useDropzone({
 		accept: { "application/pdf": [".pdf"] },
 		maxFiles: 1,
-		onDrop: (acceptedFiles) => {
+		onDrop: async (acceptedFiles) => {
 			console.log(acceptedFiles);
+			const file = acceptedFiles[0];
+			if (file.size > 10 * 1024 * 1024) {
+				alert("Please Upload a smaller files");
+				return;
+			};
+			try {
+				const data = await uploadToS3(file);
+				console.log(data, "data");
+			} catch (error) {
+				console.log({ error });
+			}
 		},
 	});
 	return (
