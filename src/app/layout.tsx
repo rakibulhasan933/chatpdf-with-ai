e2694024ef2361d063a92cd7d7e90bea85699a38
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-02 15:37:26 +0600

    [ADD] tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import ProviderLayout from '@/components/Provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <ClerkProvider>
-        <body className={inter.className}>{children}</body>
-      </ClerkProvider>
-    </html>
+    <ClerkProvider>
+      <ProviderLayout>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </ProviderLayout >
+    </ClerkProvider>
   )
 }
---
+++ src/components/Provider.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+import {
+	QueryClient,
+	QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+// Create a client
+const queryClient = new QueryClient()
+
+export default function ProviderLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<QueryClientProvider client={queryClient} >{children}</QueryClientProvider>
+	)
+}
+
