385999ef2e2c762f0d62bfd8c3aac2dd5137daf5
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-03 16:03:03 +0600

    [ADD] pdftype

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -2,6 +2,13 @@ import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { downloadPdf } from "./db/downloadpdf";
 
+type PDFPage = {
+	pageContent: string;
+	metadata: {
+		loc: { pageNumber: number };
+	};
+};
+
 export const getPineconeClient = () => {
 	return new Pinecone({
 		environment: process.env.PINECONE_ENVIRONMENT!,
@@ -12,6 +19,6 @@ export const getPineconeClient = () => {
 export async function loadIntoPinecone({ file_url }: { file_url: string }) {
 	const file = await downloadPdf({ file_url });
 	const loader = new PDFLoader(file);
-	const pages = loader.load();
+	const pages = (await loader.load()) as PDFPage[];
 	return pages;
 };
