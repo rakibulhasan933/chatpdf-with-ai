15d4e486b6e030605b04c7b4cdc8be45d01825e3
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-03 16:29:56 +0600

    [ADD] split and segment the pdf

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,10 @@
 import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { downloadPdf } from "./db/downloadpdf";
+import {
+	Document,
+	RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
 
 type PDFPage = {
 	pageContent: string;
@@ -17,8 +21,38 @@ export const getPineconeClient = () => {
 };
 
 export async function loadIntoPinecone({ file_url }: { file_url: string }) {
+
+	// 1. obtain the pdf -> downlaod and read from pdf
 	const file = await downloadPdf({ file_url });
 	const loader = new PDFLoader(file);
 	const pages = (await loader.load()) as PDFPage[];
-	return pages;
+
+	// 2. split and segment the pdf
+	const documents = await Promise.all(pages.map(prepareDocument));
+};
+
+
+
+
+// 2.
+export const truncateStringByBytes = (str: string, bytes: number) => {
+	const enc = new TextEncoder();
+	return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes))
+}
+
+async function prepareDocument(page: PDFPage) {
+	let { metadata, pageContent } = page;
+	pageContent = pageContent.replace(/\n/g, "");
+	// Split the docs
+	const splitter = new RecursiveCharacterTextSplitter();
+	const docs = await splitter.splitDocuments([
+		new Document({
+			pageContent,
+			metadata: {
+				pageNumber: metadata.loc.pageNumber,
+				text: truncateStringByBytes(pageContent, 36000)
+			},
+		}),
+	]);
+	return docs;
 };
