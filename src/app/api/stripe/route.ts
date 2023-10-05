d883a111b3cc6b6ade1aa268ba40cc50c4a6cb7f
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-05 20:47:44 +0600

    [ADD] database and payment system

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db";
+import { userSubscriptions } from "@/lib/db/schema";
+import { stripe } from "@/lib/stripe";
+import { getAuth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+const return_url = process.env.NEXT_BASE_URL + "/";
+
+export async function GET(request: NextRequest) {
+	try {
+		const { userId } = await getAuth(request);
+		if (!userId) {
+			return new NextResponse("unauthorized", { status: 401 });
+		};
+		// Database
+		const _userSubscriptions = await db.select().from(userSubscriptions).where(eq(userSubscriptions.userId, userId));
+		if (_userSubscriptions[0] && _userSubscriptions[0].stripeCustomerId) {
+			// try to cancel at the billing portal
+			const stripeSession = await stripe.billingPortal.sessions.create({
+				customer: _userSubscriptions[0].stripeCustomerId,
+				return_url,
+			});
+			return NextResponse.json({ url: stripeSession.url });
+		}
+		// User Fist time Pay
+		const stripeSession = await stripe.checkout.sessions.create
+	} catch (error) {
+
+	}
+}
