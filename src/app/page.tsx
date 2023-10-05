f98586e4f2f8d17ce2ded3decfec64657f2b6008
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-05 22:39:36 +0600

    [ADD] isPro set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import FileUpload from '@/components/FileUpload'
 import { Button } from '@/components/ui/button'
+import { db } from '@/lib/db'
+import { chats } from '@/lib/db/schema'
+import { checkSubscription } from '@/lib/subscripton'
 import { UserButton, auth } from '@clerk/nextjs/app-beta'
+import { eq } from 'drizzle-orm'
 import { ArrowRight, LogIn, BadgeSwissFranc } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function Home() {
+  // Pro Checked
   const { userId } = await auth();
-
   const isAuth = !!userId;
+  const isPro = await checkSubscription();
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    };
+  };
+
   return (
     <main className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
--- src/lib/subscripton.ts
+++ src/lib/subscripton.ts
@@ -5,7 +5,7 @@ import { eq } from "drizzle-orm";
 
 const DAY_IN_MS = 1000 * 60 * 60 * 24;
 
-export const subscription = async () => {
+export const checkSubscription = async () => {
 	const { userId } = await auth();
 	if (!userId) {
 		return false;
