805ac329eb7649faf2f62b0aa649474d77dd0c13
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-05 22:30:22 +0600

    [ADD] subscription

---
+++ src/components/SubscriptionButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SubscriptionButton() {
+	return (
+		<div>SubscriptionButton</div>
+	)
+}
---
+++ src/lib/subscripton.ts
@@ -0,0 +1,23 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "./db";
+import { userSubscriptions } from "./db/schema";
+import { eq } from "drizzle-orm";
+
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export const subscription = async () => {
+	const { userId } = await auth();
+	if (!userId) {
+		return false;
+	};
+	// Database Read
+	const _userSubscription = await db.select().from(userSubscriptions).where(eq(userSubscriptions.userId, userId));
+	if (!_userSubscription[0]) {
+		return false;
+	};
+	const userSubscription = _userSubscription[0];
+
+	const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+	return !!isValid;
+};
