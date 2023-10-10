1ad3c1715bb6080f0af16f1064a15119b2d5c3d0
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-10 23:57:46 +0600

    [ADD] layout comment removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,15 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <NextSSRPlugin
-            /**
-             * The `extractRouterConfig` will extract **only** the route configs
-             * from the router to prevent additional information from being
-             * leaked to the client. The data passed to the client is the same
-             * as if you were to fetch `/api/uploadthing` directly.
-             */
-            routerConfig={extractRouterConfig(ourFileRouter)}
-          />
+          <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           <TanStackProvider>{children}</TanStackProvider>
         </body>
         <Toaster />
