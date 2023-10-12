eeaec1082db66ebd830f7aecb271b4c67785c741
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-13 00:35:37 +0600

    [ADd] navbar and photo section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,24 @@
+import { ArrowLeft, ArrowRightCircle, ArrowRightFromLine, ArrowRightIcon } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function Home() {
 	return (
 		<div className='bg-gradient-to-bl from-green-100 via-blue-300 to-purple-300 min-h-screen'>
-			<div className="lg:mx-6 mx-2 py-4">
+			<div className="lg:mx-6 mx-2 py-2">
 				<div className="flex flex-row justify-between py-2">
 					<div className="">
 						<Link href='/' className=' font-bold text-2xl'>ChatPDF</Link>
 					</div>
 					<div className="">
-						<Link href='/' className='w-fit px-6 py-2 hover:bg-gradient-to-tl from-emerald-100 via-blue-500 to-purple-400 rounded font-medium text-lg bg-slate-50'>Login</Link>
+						<Link href={'/'} className='flex flex-row items-center  bg-white font-bold text-xl py-2 px-4 rounded hover:bg-green-400 hover:text-white'>Login<ArrowRightIcon className='w-6 font-bold ml-2' /></Link>
 					</div>
+				</div>
+				<div className=" mt-16 px-10 flex justify-center flex-col items-center">
+					<h2 className='text-5xl font-bold text-center'>Chat with any PDF</h2>
+					<Image src='/chatpdf.png' className=' object-cover mt-4 rounded-md' width={650} height={650} alt='file' />
+					<Link href={'/'} className='flex flex-row items-center mt-4 bg-white font-bold text-xl p-4 rounded hover:bg-green-400 hover:text-white'>Read Your PDF <ArrowRightCircle className='w-10 ml-2' /> </Link>
 				</div>
 			</div>
 		</div>
