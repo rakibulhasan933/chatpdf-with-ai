d4104cfa7e3bc7cfe8a51e5939e2ea88a847fd30
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-14 01:13:10 +0600

    [ADD]pricing added

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,11 +1,12 @@
 import { CheckCircle } from "lucide-react"
+import Link from "next/link"
 
 
 const includedFeatures = [
-	'Private forum access',
+	'Plus forum access',
 	'Member resources',
 	'Entry to annual conference',
-	'Official member t-shirt',
+	'Unlimited token',
 ]
 
 export default function Pricing() {
@@ -17,12 +18,9 @@ export default function Pricing() {
 				</div>
 				<div className="mx-auto max-w-2xl mt-10 rounded-3xl ring-1 ring-gray-200 sm:mt-10 lg:mx-0 lg:flex lg:max-w-none">
 					<div className="p-8 sm:p-10 lg:flex-auto">
-						<h3 className="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
-						<p className="mt-6 text-base leading-7 text-gray-600">
-							Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
-							repellendus etur quidem assumenda.
-						</p>
-						<div className="mt-10 flex items-center gap-x-4">
+						<h3 className="text-2xl font-bold tracking-tight text-gray-900">Monthly membership</h3>
+
+						<div className="mt-5 flex items-center gap-x-4">
 							<h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
 							<div className="h-px flex-auto bg-gray-100" />
 						</div>
@@ -43,15 +41,15 @@ export default function Pricing() {
 							<div className="mx-auto max-w-xs px-8">
 								<p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
 								<p className="mt-6 flex items-baseline justify-center gap-x-2">
-									<span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
+									<span className="text-5xl font-bold tracking-tight text-gray-900">$20</span>
 									<span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
 								</p>
-								<a
+								<Link
 									href="#"
 									className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 								>
 									Get access
-								</a>
+								</Link>
 								<p className="mt-6 text-xs leading-5 text-gray-600">
 									Invoices and receipts available for easy company reimbursement
 								</p>
