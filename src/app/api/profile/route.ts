9e99727b776f6b499f4a61bd968642d811c6f360
rakibulhasan933 <rakibulmd933@gmail.com>
2023-10-15 00:44:58 +0600

    [ADD] profile removed

--- src/app/api/profile/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-import { getAuth } from "@clerk/nextjs/server";
-import { clerkClient } from "@clerk/nextjs";
-import { FreeLimit } from "@/lib/userLimit";
-
-export const GET = async (req: NextRequest) => {
-
-
-}
